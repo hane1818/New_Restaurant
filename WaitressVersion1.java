88cf7bd1abd0b78f5905689c19235735cb229db8
Hane Liu <hane0131@gmail.com>
2015-06-19 17:45:49 +0800

    Without importing menus

--- WaitressVersion1.java
+++ WaitressVersion1.java
@@ -1,8 +1,5 @@
 package plfinal;
 
-import plfinal.PancakeHouseMenu;
-import plfinal.DinerMenu;
-
 public class WaitressVersion1 implements Waitress {
     private PancakeHouseMenu pancakeHouseMenu;
     private DinerMenu dinerMenu;
