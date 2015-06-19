58ad02333171620c6fa2fa6883ce7914f642e5c8
Hane Liu <hane0131@gmail.com>
2015-06-19 17:30:23 +0800

    Implement all methods of interface Waitress

--- WaitressVersion1.java
+++ WaitressVersion1.java
@@ -11,4 +11,66 @@ public class WaitressVersion1 implements Waitress {
         this.pancakeHouseMenu = pancakeHouseMenu;
         this.dinerMenu = dinerMenu;
     }
+
+    public void printMenu(){
+        printBreakfastMenu();
+        printLunchMenu();
+    }
+
+    public void printBreakfastMenu()
+    {
+        for(MenuItem m : pancakeHouseMenu.getMenuItems())
+        {
+            m.print();
+        }
+    }
+
+    public void printLunchMenu()
+    {
+        for(MenuItem m : dinerMenu.getMenuItems())
+        {
+            m.print();
+        }
+    }
+
+    public void printVegetarianMenu()
+    {
+        for(MenuItem m : pancakeHouseMenu)
+        {
+            if(isItemVegetarian(m.getName()))
+            {
+                m.print();
+            }
+        }
+
+        for(MenuItem m : dinerMenu)
+        {
+            if(isItemVegetarian(m.getName()))
+            {
+                m.print();
+            }
+        }
+    }
+
+    public boolean isItemVegetarian(String name)
+    {
+        for(MenuItem m : pancakeHouseMenu)
+        {
+            if(name.equals(m.getName()) && m.isVegetarian())
+            {
+                return true;
+            }
+        }
+
+        for(MenuItem m : dinerMenu)
+        {
+            if(name.equals(m.getName()) && m.isVegetarian())
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
