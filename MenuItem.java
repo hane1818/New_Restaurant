4cde074e63d80ccaa13154c30804c985d9fbabbb
Hane Liu <hane0131@gmail.com>
2015-06-19 14:49:41 +0800

    Initial commit

---
+++ MenuItem.java
@@ -0,0 +1,38 @@
+public class MenuItem {
+    private String name;
+    private String desciption;
+    private boolean vegetarian;
+    private double price;
+
+    public MenuItem(String name, String desciption, boolean vegetarian, double price) {
+        this.name = name;
+        this.desciption = desciption;
+        this.vegetarian = vegetarian;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return desciption;
+    }
+
+    public boolean isVegetarian(){
+        return vegetarian;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void print(){
+        System.out.print(" " + name);
+        if(vegetarian) {
+            System.out.print("(v)");
+        }
+        System.out.println(", " + price);
+        System.out.println("     -- " + description);
+    }
+}
