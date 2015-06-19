38364fa182454edba42518ba4612a306b88800e9
Hane Liu <hane0131@gmail.com>
2015-06-19 18:01:18 +0800

    Test for WaitressVersion1

--- MenuTestDrive.java
+++ MenuTestDrive.java
@@ -2,7 +2,13 @@ package plfinal;
 
 public class MenuTestDrive {
     public static void main(String[] args) {
+        /** Test for WaitressVersion1 */
+        PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+        DinerMenu dinerMenu = new DinerMenu();
 
+        Waitress waitress = new WaitressVersion1(pancakeHouseMenu, dinerMenu);
+        waitress.printMenu();
+        waitress.printVegetarianMenu();
     }
 }
 
