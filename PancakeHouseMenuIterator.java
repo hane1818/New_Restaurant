5d4e7ed27726b31160661c4d437ab507d2232809
Hane Liu <hane0131@gmail.com>
2015-06-19 18:39:30 +0800

    Implement the two methods in Iterator

--- PancakeHouseMenuIterator.java
+++ PancakeHouseMenuIterator.java
@@ -1,5 +1,22 @@
 package plfinal
 
 public class PancakeHouseMenuIterator implements Iterator {
+    private PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
+    private int index = 0;
+
+    public boolean hasNext(){
+        if(index < pancakeHouseMenu.getMenuItems().size())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public Object next(){
+        index += 1;
+
+        return pancakeHouseMenu.getMenuItems().get(index-1);
+    }
 
 }
