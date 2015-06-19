9b941bac30569b733f269db6ae59fa86158ff1ff
Hane Liu <hane0131@gmail.com>
2015-06-19 18:48:13 +0800

    Implement two methods in Iterator

--- DinerMenuIterator.java
+++ DinerMenuIterator.java
@@ -1,5 +1,22 @@
 package plfinal;
 
 public class DinerMenuIterator implements Iterator {
+    private DinerMenu dinerMenu = new DinerMenu();
+    private int index = 0;
+
+    public boolean hasNext(){
+        if(index < dinerMenu.getMenuItems().length)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public Object next(){
+        index += 1;
+
+        return dinerMenu.getMenuItems()[index-1];
+    }
 
 }
