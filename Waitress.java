82f3adb2e6d2013007b464b0ba0f7e1cd89e6287
Hane Liu <hane0131@gmail.com>
2015-06-19 17:03:26 +0800

    Add methods to this interface

--- Waitress.java
+++ Waitress.java
@@ -2,4 +2,22 @@ package plfinal;
 
 public interface Waitress {
 
+    /** prints every item on the menu */
+    public void printMenu();
+
+    /** prints just breakfast items */
+    public void printBreakfastMenu();
+
+    /** prints just lunch items */
+    public void printLunchMenu();
+
+    /** prints all vegetarian menu items */
+    public void printVegetarianMenu();
+
+    /**
+    * given the name of an item, returns true if the items is vegetarian,
+    * otherwise, returns false
+    */
+    public boolean isItemVegetarian(String name);
+
 }
