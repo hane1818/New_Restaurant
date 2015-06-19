c067edf3e355ad875db77cae462187beebd278f3
Hane Liu <hane0131@gmail.com>
2015-06-19 19:46:32 +0800

    Add constructor

--- WaitressVersion2.java
+++ WaitressVersion2.java
@@ -1,5 +1,12 @@
 package plfinal;
 
 public class WaitressVersion2 implements Waitress {
+    private PancakeHouseMenu pancakeHouseMenu;
+    private DinerMenu dinerMenu;
+
+    public WaitressVersion2(PancakeHouseMenu pancakeHouseMenu, DinerMenu dinerMenu){
+        this.pancakeHouseMenu = pancakeHouseMenu;
+        this.dinerMenu = dinerMenu;
+    }
 
 }
