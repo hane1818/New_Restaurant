332e6e0a144ed960f1d9799984691eca920373cf
Hane Liu <hane0131@gmail.com>
2015-06-19 16:33:38 +0800

    Add two fields and an initial method

--- DinerMenu.java
+++ DinerMenu.java
@@ -3,5 +3,17 @@ package plfinal
 import plfinal.MenuItem
 
 public class DinerMenu {
+    public static final int MAX_ITEMS = 6;
+    private MenuItem[] menuItems;
 
+    public DinerMenu() {
+        menuItems = new MenuItem[MAX_ITEMS];
+
+        addItem("Vegetarian BLT", "(Fakin') Bacon with lettuce & tomato on whole wheat", true, 2.99);
+        addItem("BLT", "Bacon with lettuce & tomato on whole wheat", false, 2.99);
+        addItem("Soup of the day", "Soup of the day, with a side of potato salad", false, 3.29);
+        addItem("Hotdog", "A hot dog, with saurkraut, relish, onions, topped with cheese", false, 3.05);
+        addItem("Steamed Veggies and Brown Rice", "Steamed vegetables over brown rice", true, 3.99);
+        addItem("Pasta", "Spaghetti with Marinara Sauce, and a slice of sourdough bread", false, 3.89);
+    }
 }
