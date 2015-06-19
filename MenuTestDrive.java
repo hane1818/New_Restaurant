aac53d4c8f7022291639c5ce949c41d04d523dc3
Hane Liu <hane0131@gmail.com>
2015-06-19 18:36:42 +0800

    Remove blank line at the end of file

--- MenuTestDrive.java
+++ MenuTestDrive.java
@@ -2,13 +2,15 @@ package plfinal;
 
 public class MenuTestDrive {
     public static void main(String[] args) {
-        /** Test for WaitressVersion1 */
         PancakeHouseMenu pancakeHouseMenu = new PancakeHouseMenu();
         DinerMenu dinerMenu = new DinerMenu();
 
+        /** Test for WaitressVersion1 */
         Waitress waitress = new WaitressVersion1(pancakeHouseMenu, dinerMenu);
         waitress.printMenu();
         waitress.printVegetarianMenu();
+
+        /** Test for WaitressVersion2 */
+
     }
 }
-
